94c55909316bb49ff3f94fb8aba2d1451fa2c9c4
Osorabrian <osorabrian@gmail.com>
2025-07-01 14:35:45 -0700

    create view for events list

--- unify_backend/events/views.py
+++ unify_backend/events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Event
+from .serializers import EventSerializer
 
 # Create your views here.
+class EventListView(ListView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    
